1bdd0485879e05d51aacbb1abd0b6fdbbe8dd3e1
curiousest <dmhindson@gmail.com>
2016-09-04 18:28:28 +0100

    Fixed Ubuntu pygame errors.

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -25,7 +25,8 @@ class LearningAgent(Agent):
         # TODO: Update state
         
         # TODO: Select action according to your policy
-        action = None
+        possible_actions = (None, 'forward', 'left', 'right')
+        action = possible_actions[random.randint(0,3)]
 
         # Execute action and get reward
         reward = self.env.act(self, action)
